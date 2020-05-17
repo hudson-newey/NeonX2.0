650d5772accaa0eb80999db5e8899f20cca79266
Grathium Industries <GrathiumIndustries@gmail.com>
2020-05-17 23:01:08 +1000

    color refinement on containers

---
+++ NeonX/sys/explorer.js
@@ -0,0 +1,5 @@
+let fileNavigate = (event) => {
+    if (event.keyCode == 13) {
+        window.location.href = ("http://localhost:8080?dir=" + document.getElementById('directory-search').value);
+    }
+}
