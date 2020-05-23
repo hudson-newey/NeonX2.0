851dc86ae763bfcb698c3b4868fcba6e29fd9737
Grathium Industries <GrathiumIndustries@gmail.com>
2020-05-23 13:42:40 +1000

    transitioning to typescript

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 # clean up files before push
 # or to debug
 # or just make it look pretty :)
-jsFiles=("./NeonX/sys/desktop" "./NeonX/sys/screensaver" "./NeonX/sys/script" "./NeonX/sys/login" "./NeonX/sys/explorer" "./applications/Notepad/script")
+jsFiles=("./NeonX/sys/desktop" "./NeonX/sys/screensaver" "./NeonX/sys/script" "./NeonX/sys/login" "./NeonX/sys/explorer")
 
 for i in ${jsFiles[@]}; do
     rm $i.js
