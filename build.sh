b022056943c8d2b382ecef19a4da13a2cba16102
Grathium Industries <GrathiumIndustries@gmail.com>
2020-05-23 22:56:46 +1000

    last typescript conversions

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 # build NeonX2.0 ./build.sh
 
 coffeeBuild=("./NeonX/sys/explorer")
-typescriptBuild=("./NeonX/sys/desktop" "./NeonX/sys/screensaver" "./NeonX/sys/script" "./NeonX/sys/login" "./applications/Notepad/script")
+typescriptBuild=("./NeonX/sys/desktop" "./NeonX/sys/screensaver" "./NeonX/sys/script"
+		"./NeonX/sys/login" "./applications/Notepad/script" "./applications/Calender/script")
 
 # build coffeescript files
 for c in ${coffeeBuild[@]}; do
-    rm $c.js
-    coffee -c $c.coffee
+		rm $c.js
+		coffee -c $c.coffee
 done
 
 # build typescript files
 for t in ${typescriptBuild[@]}; do
-    rm $t.js
-    tsc $t.ts
+		rm $t.js
+		tsc $t.ts
 done
