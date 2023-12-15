718e1bd84f6c85aa098c8c6b27838ab56a0cc226
hudson-newey <grath@duck.com>
2023-12-15 21:57:53 +1000

    Removed dist

--- NeonX/sys/dist/explorer.js
+++
@@ -1,6 +0,0 @@
-// search bar
-document.getElementById('directory-search').onkeyup = function (event) {
-    if (event.keyCode == 13) {
-        window.location.href = ("http://localhost:8080?dir=" + document.getElementById('directory-search').value);
-    }
-};
